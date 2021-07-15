bf12f5e96faaa6cd3a72ada7d44360a2c05dd2e4
R Williams <rjhockey1313@gmail.com>
2021-07-15 19:23:19 -0400

    Update Sprite
    Add Warrior Sprite. Define movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,10 +13,17 @@ public class PlayerMovement : MonoBehaviour
     
     private void Update()    // to access player movement, runs every frame of the game
     {
+        float horizontalInput = Input.GetAxis("Horizontal"); // store players horizontal axis vaule
+
         // to control how fast player is moving
         // Horizontal records left a key as a -1, right d key as a +1
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);  
-                                                                                    
+        body.velocity = new Vector2(horizontalInput, body.velocity.y);
+
+        // make player flip left/right
+        if (horizontalInput > .01f)   
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -.01f)
+            transform.localScale = new Vector3(-1,1,1);
 
         // Input.GetKey - boolean variable (true or false)
         // KeyCode is an enumeration that contains all buttons ie. KeyCode.space
