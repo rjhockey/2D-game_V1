4b43eaa7a52cc7870f4179135c40a7de7ed70e69
R Williams <rjhockey1313@gmail.com>
2021-07-16 20:51:35 -0400

    Player Jump & Attack
    Add attack animation and jumping parameters

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,25 +3,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float jumpPower;
+
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private Animator anim;
-    private bool grounded;
+    // private bool grounded; removed
+    private BoxCollider2D boxCollider;
+    private float wallJumpCoolDown;
+    private float horizontalInput;
 
-    private void Awake()   // runs when script is called
+    // runs when script is called
+    private void Awake()   
     {
         // grab references for rigidbody & animator from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-
-    private void Update()    // to access player movement, runs every frame of the game
+    // to access player movement, runs every frame of the game
+    private void Update()    
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); // store players horizontal axis value
+        // store players horizontal axis value
+        horizontalInput = Input.GetAxis("Horizontal"); 
 
-        // to control how fast player is moving
-        // Horizontal records left a key as a -1, right d key as a +1
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
 
         // make player flip left/right
         if (horizontalInput > .01f)
@@ -32,24 +40,78 @@ public class PlayerMovement : MonoBehaviour
         // Input.GetKey - boolean variable (true or false)
         // KeyCode is an enumeration that contains all buttons ie. KeyCode.space
         // && grounded allows player to only jump when on the ground
-        if (Input.GetKey(KeyCode.Space) && grounded)
-            Jump();
+
 
         // set animator parameters
         anim.SetBool("Run", horizontalInput != 0);
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
+
+        // wall jump logic
+        if (wallJumpCoolDown > 0.2f)
+        {
+ 
+
+            // to control how fast player is moving
+            // Horizontal records left a key as a -1, right d key as a +1
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+
+            else
+                body.gravityScale = 3;
+
+            if (Input.GetKey(KeyCode.Space))
+                Jump();
+        }
+
+        else
+            wallJumpCoolDown += Time.deltaTime;
     }
 
     private void Jump ()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);  // defines what happens when space key is depressed>player jumps
-        anim.SetTrigger("jump");
-        grounded = false;
+        if (isGrounded())
+        {
+            // defines what happens when space key is depressed>player jumps
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        else if (onWall() && !isGrounded())
+        {
+            if (horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+
+            wallJumpCoolDown = 0;
+            
+        }
+
+        
+        
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private bool isGrounded()
     {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded() && !onWall();
     }
 }
