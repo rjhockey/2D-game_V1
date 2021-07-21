4341ac3a47b5db4414aa97d68b270490f6dc3c79
R Williams <rjhockey1313@gmail.com>
2021-07-21 14:00:47 -0400

    Follow player Camera
    Camera Controller updated to follow player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // Room camera
     [SerializeField] private float speed;
     private float currentPosx;
     private Vector3 velocity = Vector3.zero;
 
+    // Follow Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
     private void Update()
     {
+        // Room camera
         // smoothdamp gradually changes position
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosx, transform.position.y, transform.position.z), ref velocity, speed);
+        // transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosx, transform.position.y, transform.position.z), ref velocity, speed);
+
+        // Follow player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
